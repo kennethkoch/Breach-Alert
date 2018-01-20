218dba7e1f4f1ed59aaec6fcc908ffa2e4ea44e6
Kenny Koch <kennykoch47@gmail.com>
2018-01-20 12:56:45 -0500

    many changes

--- src/js/background.js
+++ src/js/background.js
@@ -2,21 +2,8 @@ console.log('it works');
 
 const account = encodeURIComponent('joe@gmail.com');
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://haveibeenpwned.com/api/v2/breachedaccount/${account}`, true);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts so im safe
-    //from XSS, use https only to stop possible MITM attacks
-    //TO-DO find out how to handle 404 when acct is not found in db
-    const resp = JSON.parse(xhr.responseText)
-    console.log(resp);
-  }
-};
-xhr.send();
-
 const queryInfo = {
-  // active: true,
+  active: true,
   // currentWindow: true,
 };
 
@@ -32,10 +19,35 @@ function extractName(url) {
 }
 
 chrome.tabs.query(queryInfo, (tabs) => {
-  const tab = tabs[1];
+  const tab = tabs[0];
   console.log(tabs);
   const url = tab.url;
   console.log(url);
   const name = extractName(tab.url);
   console.log(name);
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://haveibeenpwned.com/api/v2/breach/${name}`, true);
+  xhr.onloadend = function () {
+    if (xhr.status == 404) {
+      console.log('not found');
+    }
+  };
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts so im safe
+      // from XSS, use https only to stop possible MITM attacks
+      // TO-DO find out how to handle 404 when acct is not found in db
+      const resp = JSON.parse(xhr.responseText);
+      // const description = resp.Description.replace(/ <a*>/, '')
+      // console.log(description);
+      const alertMessage = `${resp.Name} pwnCount: ${resp.PwnCount}.\n
+      Details: ${resp.Domain} was breached on ${resp.BreachDate}, affecting
+      ${resp.PwnCount} customers. The breach was made public on ${resp.AddedDate},
+       exposing ${resp.DataClasses}`;
+      console.log(resp);
+      alert(alertMessage);
+    }
+  };
+  xhr.send();
 });
+console.log(name);
