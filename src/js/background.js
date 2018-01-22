67978aa05ad0e7d5dbf1d1f3c84e61d742f0fc24
Kenny Koch <kennykoch47@gmail.com>
2018-01-22 06:55:33 -0500

    cleaned up code, removed console logs

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,3 @@
-console.log('it works');
-
-const account = encodeURIComponent('joe@gmail.com');
-
-const queryInfo = {
-  active: true,
-  // currentWindow: true,
-};
-
-// TODO clean this up
 // this takes a url and turns it into a name that can be used
 // as a param for the api call
 function extractName(url) {
@@ -15,12 +5,11 @@ function extractName(url) {
     .replace('http://', '')
     .replace('https://', '')
     .split(/[/?#]/)[0];
-  if (domain.split('.')[0] === 'www') {
-    return domain.split('.')[1];
+  if (domain.split('.').length > 2) {
+    return domain.split('.')[domain.split('.').length - 1];
   }
   return domain.split('.')[0];
 }
-
 function formatDate(date) {
   let newDate = new Date(date);
   newDate = newDate.toLocaleString();
@@ -28,7 +17,6 @@ function formatDate(date) {
 }
 
 function formatDataClasses(dataClasses) {
-  console.log(dataClasses);
   const last = dataClasses[dataClasses.length - 1];
   const rest = dataClasses.splice(0, dataClasses.length - 1);
   let result = `${rest.join(', ')} and ${last}`;
@@ -38,11 +26,7 @@ function formatDataClasses(dataClasses) {
 
 // this will update popup.js when the user switches to a different tab
 chrome.tabs.onActivated.addListener((activeInfo) => {
-  console.log(activeInfo);
   siteCheck();
-  console.log('before save');
-  // let test = chrome.storage.local.get(null);
-  // console.log(test);
 });
 
 // TO-DO update popup.js when a new url is entered
@@ -50,22 +34,22 @@ chrome.tabs.onUpdated.addListener((tab, changeInfo) => {
   siteCheck();
 });
 
+const queryInfo = {
+  active: true,
+};
 function siteCheck() {
   chrome.tabs.query(queryInfo, (tabs) => {
     const tab = tabs[0];
     const url = tab.url;
     const name = extractName(url);
-    console.log(name);
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `https://haveibeenpwned.com/api/v2/breach/${name}`, true);
     xhr.onloadend = function () {
       if (xhr.status == 404) {
-        console.log('not found');
         chrome.storage.local.set({ breachWarning: null });
         chrome.browserAction.setIcon({ path: { 19: 'images/noBreach.png' } });
         chrome.browserAction.setTitle({
-          title:
-            'No breaches found.',
+          title: 'No breaches found.',
         });
       }
     };
@@ -85,21 +69,14 @@ function siteCheck() {
           dataTypes,
           description: resp.Description,
         };
-        console.log(`pwncount:::::::${breachWarning.description}`);
         chrome.storage.local.set({ breachWarning });
         chrome.browserAction.setTitle({
           title: 'PWN ALERT! This site has been breached, click for details',
         });
         chrome.browserAction.setIcon({ path: { 19: 'images/breach.png' } });
-        console.log(breachWarning);
       }
     };
     xhr.send();
   });
 }
-
-chrome.storage.local.get((result) => {
-  console.log(result.breachWarning);
-});
-
 siteCheck();
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,4 @@
 chrome.storage.local.get({ breachWarning: 'defaultValue' }, (items) => {
-  console.log(items.breachWarning);
   if (items.breachWarning) {
     document.getElementById('breached').textContent = `${
       items.breachWarning.name
@@ -8,6 +7,7 @@ chrome.storage.local.get({ breachWarning: 'defaultValue' }, (items) => {
     document.getElementById('description').innerHTML += `<p>${items.breachWarning.description}</p>`;
   } else {
     document.getElementById('safe').innerText = 'No breaches involving this site were found.';
-    document.getElementById('noBreachMessage').innerHTML = '<p>This does not mean this site has never been breached, only that no information regarding a breach of this site has been made public at this time. You should still be sure to <span id=\'never\'>never</span> use the same password for multiple services.</p>';
+    document.getElementById('noBreachMessage').innerHTML =
+      "<p>This does not mean this site has never been breached, only that no information regarding a breach of this site has been made public at this time. You should still be sure to <span id='never'>never</span> use the same password for multiple services.</p>";
   }
 });
